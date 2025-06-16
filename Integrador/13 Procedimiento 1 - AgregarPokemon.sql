48224e23bc60813607078296ba24a0102fe876f4
Marian-Zh <msmastrocolo@gmail.com>
2025-06-16 16:04:32 -0300

    Add files via upload

---
+++ Integrador/13 Procedimiento 1 - AgregarPokemon.sql
@@ -0,0 +1,24 @@
+Create Procedure sp_AgregarPokemon ( 
+    @Nombre Varchar(100),
+    @Peso Float,
+    @Altura Float,
+	@NPokedex Int,
+	@IDRegion Int
+)
+As
+Begin
+    If Exists (
+	Select 1 
+	From Pokemon 
+	Where NPokedex = @NPokedex)
+		Begin 
+			Raiserror('Ya existe un Pokémon con ese numero de Pokédex', 16, 1);
+			Return;
+		End --para verificar si npokedex variable es igual a algun npokedex, si detecta valor 1 tira error
+
+    Insert into Pokemon(Nombre, Peso, Altura, NPokedex, IDRegion)
+    Values (@Nombre, @Peso, @Altura, @NPokedex, @IDRegion);
+End
+
+
+--EXEC sp_AgregarPokemon 'Pruebaneitor', 80, 175, 810, 9
